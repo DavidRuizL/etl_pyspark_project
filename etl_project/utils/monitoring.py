7592c7eed4313721847b2f84dd4bd509e7988edd
David Ruiz Lugo <darmando.ruiz97@gmail.com>
2024-11-15 00:23:34 -0500

    Commit final

--- etl_project/utils/monitoring.py
+++
@@ -1,6 +0,0 @@
-def validate_schema(df, expected_columns):
-    """Verifica si el DataFrame tiene las columnas esperadas."""
-    df_columns = set(df.columns)
-    missing_columns = expected_columns - df_columns
-    if missing_columns:
-        raise ValueError(f"Faltan columnas: {missing_columns}")
